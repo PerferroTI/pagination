ba06fc335819814d316c367ee8bd95271c8834b8
PerferroTI <you@example.com>
2022-02-17 14:27:25 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
-function index(props) {
-  return (
-    <div>
-      hello
-    </div>
-  );
+import {Guia} from '../components/Guia'
+import {AccordionGuia} from '../components/Acorddion'
+export default function test(props) {
+    return (
+        <div>
+            hello
+            <Guia/>
+            <AccordionGuia/>
+        </div>
+    );
 }
-
-export default index;
